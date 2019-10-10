d877a8851d891347a04166ea2c87a24040a2e7de
Emery Tate <tateejt@gmail.com>
2019-10-10 01:41:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
   if "I love you grandma".upcase
-    return "I love you too pumpkin".upcase
+    return "I love you too pumpkin!".upcase
   end
 end
     
